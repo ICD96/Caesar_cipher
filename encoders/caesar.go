6401432dbc49b3542bdf1703a72eb6dd1ea82cf7
Aziz <dead5476@gmail.com>
2024-08-01 19:35:04 +0500

    done

--- encoders/caesar.go
+++ encoders/caesar.go
@@ -16,129 +16,61 @@ func NewEncoder(lowerList, upperList string) (*Caesar, error) {
 	return &Caesar{lowerList: lowerList, upperList: upperList}, nil
 }
 
-func (e *Caesar) validEncrypt(str string, key int) (string, error) {
-	err := e.validateKey(int32(key))
+func (e *Caesar) ValidEncrypt(str string, shift int) (string, error) {
+	err := e.validateShift(int32(shift))
 	if err != nil {
 		return "", err
 	}
-	return e.encrypt(str, int32(key)), err
+	return e.encrypt(str, int32(shift)), err
 }
 
-func (e *Caesar) validateKey(key int32) error {
-	if key != 0 {
-		return errors.New("key is 0")
+func (e *Caesar) validateShift(shift int32) error {
+	if shift == 0 {
+		return errors.New("shift cannot be 0")
 	}
-	if key >= int32(len(e.lowerList)) {
-		return errors.New("key is equal to Caesar len")
+	if shift < 0 {
+		return errors.New("shift cannot be a negative number")
+	}
+	if shift == int32(len(e.lowerList)) {
+		return errors.New("shift is equal to alphabet len")
+	}
+	if shift > int32(len(e.lowerList)) {
+		return errors.New("shift is higher then alphabet len")
 	}
 	return nil
 
 }
 
-func (e *Caesar) encrypt(str string, key int32) string {
+func (e *Caesar) encrypt(str string, shift int32) string {
 	runes := []rune(str)
-	newStr := []rune{}
+	encryptedMessage := []rune{}
 	for i := 0; i < len(runes); i++ {
-		if runes[i]+key > rune(e.lowerList[len(e.lowerList)]) {
-			newStr = append(newStr, runes[i]+key-int32(len(e.lowerList)))
+		if runes[i]+shift > int32(e.lowerList[len(e.lowerList)-1]) {
+			encryptedMessage = append(encryptedMessage, runes[i]+shift-int32(len(e.lowerList)))
 		} else {
-			newStr = append(newStr, runes[i]+key)
+			encryptedMessage = append(encryptedMessage, runes[i]+shift)
 		}
 	}
-	return string(newStr)
+	return string(encryptedMessage)
 }
 
-func (e *Caesar) validDecrypt(str string, key int) (string, error) {
-	err := e.validateKey(int32(key))
+func (e *Caesar) ValidDecrypt(str string, shift int) (string, error) {
+	err := e.validateShift(int32(shift))
 	if err != nil {
 		return "", err
 	}
-	return e.decrypt(str, int32(key)), err
+	return e.decrypt(str, int32(shift)), err
 }
 
-func (e *Caesar) decrypt(str string, key int32) string {
+func (e *Caesar) decrypt(str string, shift int32) string {
 	runes := []rune(str)
-	newStr := []rune{}
+	decryptedMessage := []rune{}
 	for i := 0; i < len(runes); i++ {
-		if runes[i]+key > rune(e.lowerList[0]) {
-			newStr = append(newStr, runes[i]+key-int32(len(e.lowerList)))
+		if runes[i]-shift < int32(e.lowerList[0]) {
+			decryptedMessage = append(decryptedMessage, runes[i]-shift+int32(len(e.lowerList)))
 		} else {
-			newStr = append(newStr, runes[i]+key)
+			decryptedMessage = append(decryptedMessage, runes[i]-shift)
 		}
 	}
-	return string(newStr)
+	return string(decryptedMessage)
 }
-
-/*func (e *Caesar) Decrypt(str string, key int32) string {
-	runes := []rune(str)
-	newStr := []rune{}
-	for i := 0; i < len(runes); i++ {
-		isLower := strings.ContainsRune(e.lowerList, runes[i])
-		isUpper := strings.ContainsRune(e.upperList, runes[i])
-		if isLower {
-			if runes[i]+key > 'a' {
-				newStr = append(newStr, runes[i]-key+int32(len(e.lowerList)))
-			} else {
-				newStr = append(newStr, runes[i]-key)
-			}
-		}
-		if isUpper {
-			if runes[i]+key > 'A' {
-				newStr = append(newStr, runes[i]-key+int32(len(e.upperList)))
-			} else {
-				newStr = append(newStr, runes[i]-key)
-			}
-		}
-	}
-	return string(newStr)
-}*/
-
-/*func (e *Caesar) encrypt(str string, key int32)(string, error) {
-	runes := []rune(str)
-	newStr := []rune{}
-	for i := 0; i < len(runes); i++ {
-		isLower := strings.ContainsRune(e.lowerList, runes[i])
-		isUpper := strings.ContainsRune(e.upperList, runes[i])
-		if isLower {
-			if runes[i]+key > 'z' {
-				newStr = append(newStr, runes[i]+key-int32(len(e.lowerList)))
-			} else {
-				newStr = append(newStr, runes[i]+key)
-			}
-		}
-		if isUpper {
-			if runes[i]+key > al[len(alp)-1] { // Поставить условие если ключ + наш символ больше чем наш алфавит
-				newStr = append(newStr, runes[i]+key-int32(len(e.upperList)))
-			} else {
-				newStr = append(newStr, runes[i]+key)
-			}
-		}
-	}
-	return string(newStr)
-}*/
-
-//
-//[0]-[len-1]
-// list  (a b c )
-// 15 14 51
-
-// asic
-
-// min max  int8
-
-//    +1
-//   a b c
-//
-
-// a +1
-// c +1
-//  c + len(alp) = ошибка
-
-//  key   key + symb < len
-
-//  key + symb > len
-
-// key  0
-// ket  len
-// key  > len -  err
-// ket -1
--- main.go
+++ main.go
@@ -16,18 +16,28 @@ func main() {
 		return
 	}
 
-	var encryptedMessage, decryptedMessage string
-	var key int
+	var messageToEncrypt, messageToDecrypt, encryptedMessage, decryptedMessage string
+	var shift int
 
-	pflag.StringVarP(&encryptedMessage, "encrypt", "e", "", "Шифрование соообщения")
-	pflag.StringVarP(&decryptedMessage, "decrypt", "d", "", "Дешифрование соообщения")
-	pflag.IntVarP(&key, "key", "k", 0, "Ключ сдвига при шифровании")
-
+	pflag.StringVarP(&messageToEncrypt, "encrypt", "e", "", "Шифрование соообщения по ключу")
+	pflag.StringVarP(&messageToDecrypt, "decrypt", "d", "", "Дешифрование соообщения по ключу")
+	pflag.IntVarP(&shift, "shift", "s", 0, "Ключ сдвига при шифровании")
 	pflag.Parse()
 
-	//TODO here
-	encoders.NewCaesarEncoder()
-	if len(encryptedMessage) != 0 {
-		fmt.Println()
+	if len(messageToEncrypt) != 0 {
+		encryptedMessage, err = encoder.ValidEncrypt(messageToEncrypt, shift)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(encryptedMessage)
+	}
+	if len(messageToDecrypt) != 0 {
+		decryptedMessage, err = encoder.ValidDecrypt(messageToDecrypt, shift)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(decryptedMessage)
 	}
 }
